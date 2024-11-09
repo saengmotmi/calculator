7cb9c9f15f982fa8e991079c4a494455c37cff37
saengmotmi <ohjtack@gmail.com>
2024-11-10 00:37:48 +0900

    chore: vite, react

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+
+import CalculatorPanel from "./widget/CalculatorPanel";
+
+createRoot(document.getElementById("root")!).render(<CalculatorPanel />);
---
+++ src/widget/CalculatorPanel.tsx
@@ -0,0 +1,9 @@
+const CalculatorPanel = () => {
+  return (
+    <div>
+      <h1>Calculator Panel</h1>
+    </div>
+  );
+};
+
+export default CalculatorPanel;

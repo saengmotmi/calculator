6fed558ebd384a51f72357054ae3d5d38213c010
saengmotmi <ohjtack@gmail.com>
2024-11-09 17:22:34 +0900

    feature: undo 구현

--- src/features/calculator/ArithmeticCalculator.ts
+++ src/features/calculator/ArithmeticCalculator.ts
@@ -1,3 +1,4 @@
+// ArithmeticCalculator.ts
 import { Infix } from "../../entities/notations/infix";
 import { Notation } from "../../entities/notations/notation";
 import { Postfix } from "../../entities/notations/postfix";
@@ -5,35 +6,38 @@ import { Prefix } from "../../entities/notations/prefix";
 import { ArithmeticEvaluator } from "../../entities/operations/arithmetics";
 
 export class ArithmeticCalculator {
-  private expression: string = "";
+  private inputHistory: string[] = [];
 
   inputNumber(number: string | number) {
-    this.expression += ` ${number}`;
+    this.inputHistory.push(`${number}`);
   }
 
   inputOperator(operator: string) {
-    this.expression += ` ${operator}`;
+    this.inputHistory.push(`${operator}`);
   }
 
   inputParenthesis(paren: "(" | ")") {
-    this.expression += ` ${paren}`;
+    this.inputHistory.push(paren);
   }
 
   clear() {
-    this.expression = "";
+    this.inputHistory = [];
+  }
+
+  undo() {
+    this.inputHistory.pop();
   }
 
   calculate() {
-    const calculator = ArithmeticCalculatorFactory.create(
-      this.expression.trim()
-    );
+    const expression = this.inputHistory.join(" ");
+    const calculator = ArithmeticCalculatorFactory.create(expression.trim());
     const result = calculator.evaluate();
     this.clear(); // 계산 후 초기화
     return result;
   }
 
   getExpression() {
-    return this.expression.trim();
+    return this.inputHistory.join(" ").trim();
   }
 }
 
